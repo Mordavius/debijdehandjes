53d1dd81fd9fb19fb6cffb4dcbf0844d8ccd6fae
Girgis Iskondos <g.w.n.h.iskondos@gmail.com>
2017-11-01 15:04:30 +0100

    periodicall save

---
+++ prototype/Assets/scripts/SeedPlacement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedPlacement : MonoBehaviour {
+    public GameObject gevuld;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Seed")
+        {
+            Instantiate(gevuld, transform.position, transform.rotation);
+            Destroy(other.gameObject);
+        }
+    }
+}
