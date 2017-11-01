0f33a701f59347589a827f083f1bce09d6364df1
Girgis Iskondos <g.w.n.h.iskondos@gmail.com>
2017-11-01 13:36:14 +0100

    first test

---
+++ prototype/Assets/scripts/SquareEnter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareEnter : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Square")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
