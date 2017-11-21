7e48ba67c7644679a5cc42c3200cc67439181f24
Girgis Iskondos <g.w.n.h.iskondos@gmail.com>
2017-11-21 13:21:41 +0100

    added loadscenes script

---
+++ prototype/Assets/scripts/prototype2/LoadScenes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void loadMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public static void loadGarden()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public static void loadPump()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public static void loadPuzzle()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
