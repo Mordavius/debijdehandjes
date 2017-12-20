4a3d7b2767dc902d54d9a740240c4ceb5c3f0727
Eva <_Eeef@live.nl>
2017-12-20 13:14:50 +0100

    Half implementation of score in GardenLevel

---
+++ prototype/Assets/Resources/Classes/Garden/ScoreHarvest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreHarvest : MonoBehaviour {
+
+    private int scoreCount = 0;
+    public Text scoreText;
+    private Plant plant;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != "Seed")
+        {
+
+            scoreCount += plant.valueOfHarvest;
+            scoreText.text = scoreCount.ToString();
+            Destroy(other.gameObject);
+        }
+    }
+}
