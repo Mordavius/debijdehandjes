4f036c3bf6364a73f15c46f132c7af8df215a47a
Mordavius <g.w.n.h.iskondos@gmail.com>
2017-12-21 13:28:09 +0100

    got pump working for both sides

---
+++ prototype/Assets/Resources/Classes/Pump/ArmSelection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmSelection : MonoBehaviour {
+    public bool left = false;
+    public bool right = false;
+    GameObject canvas;
+    GameObject flip;
+
+    public void Start()
+    {
+        canvas = GameObject.Find("StartCanvas");
+        flip = GameObject.Find("Flip");
+    }
+
+    public void SelectLeft()
+    {
+
+        canvas.SetActive(false);
+        left = true;
+    }
+
+    public void SelectRight()
+    {
+        canvas.SetActive(false);
+        right = true;
+        flip.transform.localScale = new Vector3(-1, 1, 1);
+    }
+}
--- prototype/Assets/Resources/Classes/Pump/RotateHandle.cs
+++ prototype/Assets/Resources/Classes/Pump/RotateHandle.cs
@@ -10,11 +10,16 @@ public class RotateHandle : MonoBehaviour {
     private Vector3 mouseOffset;
     private Vector3 rotation;
     private bool isRotating;
+    ArmSelection selectedArm;
+    GameObject canvas;
 
     void Start()
     {
         sensitivity = 0.11f;
         rotation = Vector3.zero;
+        canvas = GameObject.Find("StartCanvas");
+        selectedArm = (ArmSelection)canvas.GetComponent(typeof(ArmSelection));
+ 
     }
 
     void Update()
@@ -24,9 +29,43 @@ public class RotateHandle : MonoBehaviour {
             // offset
             mouseOffset = (Input.mousePosition - mouseReference);
             // apply rotation
-            rotation.z = -(mouseOffset.y) * sensitivity;
+            if (selectedArm.left)
+            {
+                rotation.z = -(mouseOffset.y) * sensitivity;
+            }
+            else if (selectedArm.right)
+            {
+                rotation.z = +(mouseOffset.y) * sensitivity;
+            }
             // rotate
             //transform.Rotate(rotation);
+            if (selectedArm.left)
+            {
+                if (gameObject.transform.rotation.eulerAngles.z <= 5)
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 5.1f);
+                    Debug.Log(gameObject.transform.rotation.eulerAngles.z);
+                }
+                else if (gameObject.transform.rotation.eulerAngles.z >= 81)
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 80.9f);
+                }
+            }
+            if (selectedArm.right)
+            {
+                if (gameObject.transform.localRotation.eulerAngles.z <= 4.6f)
+                {
+                    transform.eulerAngles = new Vector3(0, 0, -4.5f);
+                    Debug.Log(gameObject.transform.localRotation.eulerAngles.z);
+                    //rotation.z = 5f;
+                }
+                else if (gameObject.transform.localRotation.eulerAngles.z >= 82)
+                {
+                    transform.eulerAngles = new Vector3(0, 0, -82.2f);
+                    //rotation.z = 81f;
+                }
+
+            }
             transform.eulerAngles += rotation;
             // store mouse
             mouseReference = Input.mousePosition;
