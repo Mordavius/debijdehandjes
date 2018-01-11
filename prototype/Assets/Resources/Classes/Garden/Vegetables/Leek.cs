7c21b0166f79d159e6c24acc62aa980d55b23bc4
rubendroogh <rubendroogh@hotmail.nl>
2018-01-11 09:31:37 +0100

    Balancing

--- prototype/Assets/Resources/Classes/Garden/Vegetables/Leek.cs
+++ prototype/Assets/Resources/Classes/Garden/Vegetables/Leek.cs
@@ -6,5 +6,5 @@ public class Leek : Plant
 {
     public override string plantName { get { return "leek"; } }
     public override int valueOfHarvest { get { return 12; } }
-    public override float growthMultiplier { get { return 0.5f; } }
+    public override float growthMultiplier { get { return 0.8f; } }
 }
